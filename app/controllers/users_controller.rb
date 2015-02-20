e8b6564beca6fe7e191c273ab5239b22659ea82d
7sun <pakrouse@gmail.com>
2015-02-20 10:55:14 -0800

    Adds users controller, builds user 'new' and 'index' views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  def index
+    # Should only be visible by admin.
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      if @user.save
+        # temporary path until landing page is built
+        redirect_to users_path
+      else
+        render :new
+      end
+    end
+
+  def show
+    @user = User.find(:id)
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'application#index'
+
+  resources :users
+  
 end
