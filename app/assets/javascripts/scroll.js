edc7f6575ad010c6db8d9e3fad45a6a6400f535b
dmaupin <dm@davidmaupin.com>
2015-02-25 13:22:37 -0800

    basic nav collapse added

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -24,3 +24,19 @@
 jQuery(function(){  
   jQuery.mark.jump();
 });
+
+// Menu Collapse
+
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
