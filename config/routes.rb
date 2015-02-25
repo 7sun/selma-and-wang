88a5c5ff999c262161c76f779efacf25a89cf2c7
7sun <pakrouse@gmail.com>
2015-02-24 19:48:31 -0800

    adds soundcloud index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   root 'application#index'
 
-  get '/users'     =>  'users#index', as: 'users'
-  get '/signup'    => 'users#new', as: 'signup'
-  post '/signup'   => 'users#create'
-  get '/login'     => 'sessions#new', as: 'login'
-  post '/login'    => 'sessions#create'
-  get '/logout'    => 'sessions#destroy', as: 'logout'
+  get '/users'        =>  'users#index', as: 'users'
+  get '/signup'       => 'users#new', as: 'signup'
+  post '/signup'      => 'users#create'
+  get '/login'        => 'sessions#new', as: 'login'
+  post '/login'       => 'sessions#create'
+  get '/logout'       => 'sessions#destroy', as: 'logout'
+  get 'music-player'  => 'soundcloud#index', as: 'soundcloud'
+
   get '/auth/:provider/callback', to: 'sessions#create_fb'
 
   # resources :users
