5b4340eeaba73dc9111c4158f9cb342978b80a4e
dmaupin <dm@davidmaupin.com>
2015-02-27 13:58:31 -0800

    UI updated

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,38 +1,38 @@
 // In Page Scrolling
 
-// (function (jQuery) {
-//   jQuery.mark = {
-//     jump: function (options) {
-//       var defaults = {
-//         selector: 'a.scroll-on-page-link'
-//       };
-//       if (typeof options == 'string') defaults.selector = options;
-//       var options = jQuery.extend(defaults, options);
-//       return jQuery(options.selector).click(function (e) {
-//         var jumpobj = jQuery(this);
-//         var target = jumpobj.attr('href');
-//         var thespeed = 725;
-//         var offset = jQuery(target).offset().top;
-//         jQuery('html,body').animate({
-//           scrollTop: offset
-//         }, thespeed, 'swing')
-//         e.preventDefault();
-//       })
-//     }
-//   }
-// })(jQuery);
+(function (jQuery) {
+  jQuery.mark = {
+    jump: function (options) {
+      var defaults = {
+        selector: 'a.scroll-on-page-link'
+      };
+      if (typeof options == 'string') defaults.selector = options;
+      var options = jQuery.extend(defaults, options);
+      return jQuery(options.selector).click(function (e) {
+        var jumpobj = jQuery(this);
+        var target = jumpobj.attr('href');
+        var thespeed = 725;
+        var offset = jQuery(target).offset().top;
+        jQuery('html,body').animate({
+          scrollTop: offset
+        }, thespeed, 'swing')
+        e.preventDefault();
+      })
+    }
+  }
+})(jQuery);
 
-// jQuery(function(){  
-//   jQuery.mark.jump();
-// });
+jQuery(function(){  
+  jQuery.mark.jump();
+});
 
 // Alt Scroll Func 
 
-$("#a.scroll-on-page-link").click(function() {
-    $('html, body').animate({
-        scrollTop: $('href').offset().top
-    }, 2000);
-});
+// $("#a.scroll-on-page-link").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $('href').offset().top
+//     }, 2000);
+// });
 
 // Menu Collapse
 
