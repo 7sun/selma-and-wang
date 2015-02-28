72fcc023ef5f195b15a4636e4b535a94d239ce67
7sun <pakrouse@gmail.com>
2015-02-28 13:36:40 -0800

    changes user dream patch to an ng-repeat

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -1,10 +1,45 @@
 angular
 	.module("SelmaApp", ['firebase'])
-	.controller("tileController", ['$scope', '$firebase', function($scope, $firebase){
-		  var ref = new Firebase("https://selmaandwang.firebaseio.com/tiles");
-  		var sync = $firebase(ref);
+	.controller("patchController", ['$scope', '$firebase', function($scope, $firebase){
+			var squares = $firebase(new Firebase("https://selmaandwang.firebaseio.com/patches/squares")).$asArray();
 
-  		sync.$set({foo: "bar"});
+			squares.$loaded().then(function(){
+				console.log("loaded record: ", squares);
+			})
+
+  		// patches.$bindTo($scope, 'patches');
+  		// console.log("patches: " + patches);
+
+
+  		$scope.addSquareColor = addSquareColor;
+  		$scope.squares = [{topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}, {topColor: "", rightColor: ""},
+								{topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}]
+
+  	// Adds color to half of the square. Adds color to each half before increasing the count to the next integer
+	  function addSquareColor(color){
+	    var clickCountString = Math.floor(clickCount).toString();
+	    if (clickCount % 1 == 0.5){
+	      $('#answer-square-' + clickCountString).css({"borderRightColor": color});
+	      $scope.squares[Math.floor(clickCount)].rightColor = color;
+	      console.log(clickCount);
+	        if (clickCount >= 5.5){
+	          $('#save').removeClass('hidden');
+	          console.log($scope.squares);
+	        }
+	    } else if (clickCount % 1 == 0){
+	      $('#answer-square-' + clickCountString).css({"borderTopColor": color});
+	      $scope.squares[Math.floor(clickCount)].topColor = color;
+	      console.log(clickCount);
+	    }
+
+
+	  }
+
+
+
+
+
+
 
 
 	}]);
