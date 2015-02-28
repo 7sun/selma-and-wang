c1f36af450ca649e759848d19ca50dbc67b436bb
7sun <pakrouse@gmail.com>
2015-02-28 14:56:56 -0800

    creates quilt of all user patches

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -1,38 +1,34 @@
 angular
 	.module("SelmaApp", ['firebase'])
 	.controller("patchController", ['$scope', '$firebase', function($scope, $firebase){
-			var squares = $firebase(new Firebase("https://selmaandwang.firebaseio.com/patches/squares")).$asArray();
+			var ref = new Firebase("https://selmaandwang.firebaseio.com/patches");
+			var patches = $firebase(ref).$asObject();
+			 patches.$bindTo($scope, 'patches');
+  		console.log("patches: " + patches);
 
-			squares.$loaded().then(function(){
-				console.log("loaded record: ", squares);
-			})
-
-  		// patches.$bindTo($scope, 'patches');
-  		// console.log("patches: " + patches);
 
 
   		$scope.addSquareColor = addSquareColor;
-  		$scope.squares = [{topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}, {topColor: "", rightColor: ""},
+  		var dbsquares = [{topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}, {topColor: "", rightColor: ""},
 								{topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}]
-
+  		$scope.squares = [1,2,3,4,5,6];
+  		
   	// Adds color to half of the square. Adds color to each half before increasing the count to the next integer
 	  function addSquareColor(color){
 	    var clickCountString = Math.floor(clickCount).toString();
-	    if (clickCount % 1 == 0.5){
-	      $('#answer-square-' + clickCountString).css({"borderRightColor": color});
-	      $scope.squares[Math.floor(clickCount)].rightColor = color;
-	      console.log(clickCount);
-	        if (clickCount >= 5.5){
-	          $('#save').removeClass('hidden');
-	          console.log($scope.squares);
-	        }
-	    } else if (clickCount % 1 == 0){
+	    if (clickCount % 1 == 0){
 	      $('#answer-square-' + clickCountString).css({"borderTopColor": color});
-	      $scope.squares[Math.floor(clickCount)].topColor = color;
+	      dbsquares[Math.floor(clickCount)].topColor = color;
+	      console.log(clickCount); 
+	    } else if (clickCount % 1 == 0.5){
+	      $('#answer-square-' + clickCountString).css({"borderRightColor": color});
+	      dbsquares[Math.floor(clickCount)].rightColor = color;
 	      console.log(clickCount);
+        if (clickCount >= 5.5){
+          $('#save').removeClass('hidden');
+          ref.push(dbsquares);
+        }
 	    }
-
-
 	  }
 
 
