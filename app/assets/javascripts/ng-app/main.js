bb05c5f4f07c977cf424f37b3992c3272acb0aaa
7sun <pakrouse@gmail.com>
2015-03-02 00:13:56 -0800

    Changes quilt pattern back to central diamond. Adds optional code for multiple diamond patterns.

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -12,7 +12,7 @@ angular
   		var dbsquares = [{topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}, {topColor: "", rightColor: ""},
 								{topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}, {topColor: "", rightColor: ""}]
   		$scope.squares = [1,2,3,4,5,6];
-  		
+
   	// Adds color to half of the square. Adds color to each half before increasing the count to the next integer
 	  function addSquareColor(color){
 	    var clickCountString = Math.floor(clickCount).toString();
@@ -25,7 +25,8 @@ angular
 	      dbsquares[Math.floor(clickCount)].rightColor = color;
 	      console.log(clickCount);
         if (clickCount >= 5.5){
-          $('#save').removeClass('hidden');
+        	$('#save').removeClass('hidden');
+          $('#dream-patch').removeClass('hidden');
           ref.push(dbsquares);
         }
 	    }
