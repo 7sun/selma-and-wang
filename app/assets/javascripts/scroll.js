4db578bb65f202d979583ed625c89d59c6e971cf
dmaupin <dm@davidmaupin.com>
2015-03-03 11:05:41 -0800

    merged

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,38 +1,35 @@
 // In Page Scrolling
 
-(function (jQuery) {
-  jQuery.mark = {
-    jump: function (options) {
-      var defaults = {
-        selector: 'a.scroll-on-page-link'
-      };
-      if (typeof options == 'string') defaults.selector = options;
-      var options = jQuery.extend(defaults, options);
-      return jQuery(options.selector).click(function (e) {
-        var jumpobj = jQuery(this);
-        var target = jumpobj.attr('href');
-        var thespeed = 725;
-        var offset = jQuery(target).offset().top;
-        jQuery('html,body').animate({
-          scrollTop: offset
-        }, thespeed, 'swing')
-        e.preventDefault();
-      })
-    }
-  }
-})(jQuery);
+// (function (jQuery) {
+//   jQuery.mark = {
+//     jump: function (options) {
+//       var defaults = {
+//         selector: 'a.scroll-on-page-link'
+//       };
+//       if (typeof options == 'string') defaults.selector = options;
+//       var options = jQuery.extend(defaults, options);
+//       return jQuery(options.selector).click(function (e) {
+//         var jumpobj = jQuery(this);
+//         var target = jumpobj.attr('href');
+//         var thespeed = 725;
+//         var offset = jQuery(target).offset().top;
+//         jQuery('html,body').animate({
+//           scrollTop: offset
+//         }, thespeed, 'swing')
+//         e.preventDefault();
+//       })
+//     }
+//   }
+// })(jQuery);
 
-jQuery(function(){  
-  jQuery.mark.jump();
-});
+// jQuery(function(){  
+//   jQuery.mark.jump();
+// });
 
-
+// 
 
 $("#button-down").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#scroll-link-4").offset().top
-    }, 725);
-    console.log("yup")
+  alert( "Handler for .click() called." );
 });
 
 // $(document).ready(function (){
