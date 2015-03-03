b331ff5f1a00ac02d85c2d2b87fe53e78d096ab3
7sun <pakrouse@gmail.com>
2015-03-02 16:42:16 -0800

    patches now alternate to match quilt.

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -34,12 +34,12 @@ angular
   		function setTransparentTriangles(){
   			console.log("outside func counter val: " + counter);
 	    	if ( between(counter, 1, 3) || between(counter, 7, 9) || between(counter, 16, 18) || between(counter, 22, 24) ){
-	    		$('#answer-square-1').css({"borderTopColor": "transparent"});
-	    		$('#answer-square-6').css({"borderRightColor": "transparent"});
+	    		$('#answer-square-0').css({"borderTopColor": "#A0E18F"});
+	    		$('.answer-square').css({"borderRight": "250px solid #A0E18F"});
 	    		inverse = false;
 	    	} else {
-	    		$('#answer-square-2').css({"borderTopColor": "transparent"});
-	    		$('#answer-square-5').css({"borderLeftColor": "transparent"});
+	    		$('#answer-square-1').css({"borderTopColor": "#A0E18F"});
+	    		$('.answer-square').css({"borderLeft": "250px solid #A0E18F"});
 	    		inverse = true;
 	    	}
 	    	setInverse(inverse)
@@ -60,11 +60,12 @@ angular
 	    		$('#answer-square-' + clickCountString).css({"borderLeft": "250px solid " + color});
 	    		dbsquares[Math.floor(clickCount)].sideColor = color;
 	    	} else {
-	    		$('#answer-square-' + clickCountString).css({"borderRight": "250px solid transparent"});
+	    		// $('#answer-square-' + clickCountString).css({"borderRight": "250px solid transparent"});
 		      $('#answer-square-' + clickCountString).css({"borderTop": "250px solid " + color});
 		      dbsquares[Math.floor(clickCount)].topColor = color;
 	    	}
 	      console.log(clickCount);
+	      checkForCompletePatch();
 	      // Checks if clickCount end in 0.5. If so, the squares bottom/right triangle color is set 
 	    } else if (clickCount % 1 == 0.5){
 	    	if (inverse){
@@ -75,14 +76,30 @@ angular
 	      	dbsquares[Math.floor(clickCount)].sideColor = color;
 	      }
 	      console.log(clickCount);
-	      // Checks if patch is complete, then shows save button and patch.
-        if (clickCount >= 5.5){
-        	$('#save').removeClass('hidden');
-          $('#dream-patch').removeClass('hidden');
-          ref.push(dbsquares);
-        }
+	      checkForCompletePatch();
+      }
+	  }
+
+    // Checks if patch is complete, then shows save button and patch.
+	  function checkForCompletePatch(){
+	    if (clickCount >= 5.5 || (clickCount >= 5 && inverse == false) ){
+	    	$('#save').removeClass('hidden');
+	      $('#dream-patch').removeClass('hidden');
+	      ref.push(dbsquares);
 	    }
 	  }
+
+		// Hides question square after user clicks a square and increases click count.
+	  $('.square').click(function(){
+	    $('#questions').addClass('hidden');
+	    if (inverse && (clickCount == 1.0 || clickCount == 3.5)){
+	      clickCount += 1.0;
+	    } else if (inverse == false && (clickCount == -0.5 || clickCount == 5.0)){
+	    	clickCount += 1.0;
+	    } else {
+	      clickCount += 0.5;
+	    }
+	  })
 
 
 	}]);
