71d95e25a853ee138a519b77c897a2a44cc2ce45
gogojojo <joseph.gochua@gmail.com>
2015-03-04 14:49:24 -0800

    removing unessential files

--- app/mailers/user_notifier.rb
+++
@@ -1,9 +0,0 @@
-class UserNotifier < ApplicationMailer
-  default :from => 'selmaand@wang.com'
-
-  def send_signup_email(user)
-    @user = user
-    mail( :to => @user.email,
-    :subject => 'Thanks for Signing up!'
-  end
-end
