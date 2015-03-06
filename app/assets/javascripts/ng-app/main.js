291e635019ec895ca9fa0a187d4781ce27b1654e
7sun <pakrouse@gmail.com>
2015-03-06 12:39:58 -0800

    adds falling divs

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -1,6 +1,10 @@
 angular
 	.module("SelmaApp", ['firebase'])
 	.controller("patchController", ['$scope', '$firebase', function($scope, $firebase){
+		// $scope.startLeaf = startLeaf;
+		// $scope.startLeaf = stopLeaf;
+		// $scope.leafUpdate = leafUpdate;
+
 		$scope.squares = [1,2,3,4,5,6];
 		$scope.addSquareColor = addSquareColor;
 		$scope.togglePlay = togglePlay;
@@ -148,5 +152,183 @@ angular
 	  	$('.play-icon').toggleClass('fa-play');
 	  	widget.toggle();
 	  }
+
+
+	  // ////////////////////////////////////////Floating leaf animation test
+
+		// var isUpdatingLeaf = false;
+		// var velocityPerFrame = 2;       // in pixels per tick
+		// var leftBound = 500;            // left boundry of parent, in pixels
+		// var zagAmplitude = 100;         // in total width of zigzag motion
+		// var totalSteps = 0;
+
+		// function startLeaf() {
+		//     isUpdatingLeaf = true;
+		//     leafUpdate();
+		//     console.log("start ran");
+		// }
+
+		// function stopLeaf() {
+		//     isUpdatingLeaf = false;
+		//     console.log("stop ran");
+		// }
+
+		// function leafUpdate() {
+		//     totalSteps += 1; // increase our angle counter
+		//     var leaf = $('#leaf'); // grab our leaf using jQuery
+		//     var leafx = leaf.position().top + velocityPerFrame; // move the leaf down by our velocity
+		//     var leafy = Math.cos(totalSteps / 1000 * Math.PI * 2) * zagAmplitude + leftBound; // use a cosine to model it's left right behavior
+		//     leaf.css({top: leafx + 'px', left: leafy + 'px'});  // update the leaf's css to match
+
+		//     if(isUpdatingLeaf) {
+		//         // still updating? let's refresh in 33 miliseconds
+		//         setTimeout(leafUpdate, 33);
+		//     }
+		//     console.log("update ran");
+		// }
+
+		// startLeaf();
+
+	
+/* Define the number of leaves to be used in the animation */
+const NUMBER_OF_LEAVES = 100;
+
+/* 
+    Called when the "Falling Leaves" page is completely loaded.
+*/
+function init()
+{
+    /* Get a reference to the element that will contain the leaves */
+    var container = document.getElementById('leafContainer');
+    /* Fill the empty container with new leaves */
+    for (var i = 0; i < NUMBER_OF_LEAVES; i++) 
+    {
+        container.appendChild(createALeaf());
+    }
+}
+
+
+/*
+    Receives the lowest and highest values of a range and
+    returns a random integer that falls within that range.
+*/
+function randomInteger(low, high)
+{
+    return low + Math.floor(Math.random() * (high - low));
+}
+
+
+/*
+   Receives the lowest and highest values of a range and
+   returns a random float that falls within that range.
+*/
+function randomFloat(low, high)
+{
+    return low + Math.random() * (high - low);
+}
+
+
+/*
+    Receives a number and returns its CSS pixel value.
+*/
+function pixelValue(value)
+{
+    return value + 'px';
+}
+
+
+/*
+    Returns a duration value for the falling animation.
+*/
+
+function durationValue(value)
+{
+    return value + 's';
+}
+
+
+/*
+    Uses an img element to create each leaf. "Leaves.css" implements two spin 
+    animations for the leaves: clockwiseSpin and counterclockwiseSpinAndFlip. This
+    function determines which of these spin animations should be applied to each leaf.
+    
+*/
+function createALeaf()
+{
+    /* Start by creating a wrapper div, and an empty img element */
+    var leafDiv = document.createElement('div');
+    var image = document.createElement('div');
+    
+    /* Randomly choose a leaf image and assign it to the newly created element */
+    // image.src = 'images/realLeaf' + randomInteger(1, 5) + '.png';
+    
+    leafDiv.style.top = (Math.floor(Math.random() * (-1400 + -100) -100)).toString() + "px"
+
+    /* Position the leaf at a random location along the screen */
+    leafDiv.style.left = pixelValue(randomInteger(0, 1400));
+    
+    /* Randomly choose a spin animation */
+    var spinAnimationName = (Math.random() < 0.5) ? 'clockwiseSpin' : 'counterclockwiseSpinAndFlip';
+    
+    /* Set the -webkit-animation-name property with these values */
+    leafDiv.style.webkitAnimationName = 'fade, drop';
+    image.style.webkitAnimationName = spinAnimationName;
+    
+    /* Figure out a random duration for the fade and drop animations */
+    var fadeAndDropDuration = durationValue(randomFloat(42, 64));
+    
+    /* Figure out another random duration for the spin animation */
+    var spinDuration = durationValue(randomFloat(4, 8));
+    /* Set the -webkit-animation-duration property with these values */
+    leafDiv.style.webkitAnimationDuration = fadeAndDropDuration + ', ' + fadeAndDropDuration;
+
+    var leafDelay = durationValue(randomFloat(0, 5));
+    leafDiv.style.webkitAnimationDelay = leafDelay + ', ' + leafDelay;
+
+    image.style.webkitAnimationDuration = spinDuration;
+
+    // add the <img> to the <div>
+    leafDiv.appendChild(image);
+
+    /* Return this img element so it can be added to the document */
+    return leafDiv;
+}
+
+
+/* Calls the init function when the "Falling Leaves" page is full loaded */
+window.addEventListener('load', init, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	}]);
