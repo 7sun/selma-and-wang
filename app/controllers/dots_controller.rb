c39a7b31dd7f4cc369a394811235a8d7783fffed
gogojojo <joseph.gochua@gmail.com>
2015-03-09 14:04:41 -0700

    adding comments to dot controller

--- app/controllers/dots_controller.rb
+++ app/controllers/dots_controller.rb
@@ -1,5 +1,7 @@
 class DotsController < ApplicationController
+  #to keep track of clicks per dot
   def counter
+  # if the dot color doesn't exist create a row in the db for it and add one
     if Dot.find_by(dotid: params[:param1]) === nil
       @dot = Dot.new
       @dot.dotid = (params[:param1]) 
@@ -7,6 +9,7 @@ class DotsController < ApplicationController
       @dot.save
       
     else
+  # if the dot exists add one to the count
       @dot = Dot.find_by(dotid: params[:param1])
       @dot.count += 1
       @dot.save
