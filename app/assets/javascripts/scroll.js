48229760e0146bcc162f1f7be88f5ec50b39604c
dmaupin <dm@davidmaupin.com>
2015-03-11 10:34:05 -0700

    updated comments on in page scrolling

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -33,10 +33,10 @@ $(document).ready(function() {
      sectionContainer: "article",     // sectionContainer accepts any kind of selector in case you don't want to use section
      easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
                                       // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-     animationTime: 725,             // AnimationTime let you define how long each section takes to animate
+     animationTime: 725,              // AnimationTime let you define how long each section takes to animate
      pagination: false,               // You can either show or hide the pagination. Toggle true for show, false for hide.
      updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-     afterMove: function(index) {
+     afterMove: function(index) {     // This option accepts a callback function. The function will be called before the page moves.
       if (index == 1) {
         $("body").css("background-color","#A0E18F");
       } if (index == 2) {
@@ -51,8 +51,8 @@ $(document).ready(function() {
         $("body").css("background-color","#e1b888");
       }
       console.log(index);
-     },                               // This option accepts a callback function. The function will be called before the page moves.
-     beforeMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+     },                               
+     beforeMove: function(index) {},  // This option accepts a callback function. The function will be called after the page moves.
      loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
      keyboard: true,                  // You can activate the keyboard controls
      responsiveFallback: false,       // You can fallback to normal page scroll by defining the width of the browser in which
@@ -66,6 +66,12 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $( "#going-down" ).click(function() {
+    $(".main").moveDown();
+  });
+});
+
+$(document).ready(function() {
+  $( "#to-quilt" ).click(function() {
     $(".main").moveDown();
   });
 });
