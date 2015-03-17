a2565048fbc4bbda55ac3c317a558cf5704ce987
gogojojo <joseph.gochua@gmail.com>
2015-03-17 11:52:03 -0700

    one more time for circle ci

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -9,12 +9,5 @@ RSpec.describe NotesController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 
 end
