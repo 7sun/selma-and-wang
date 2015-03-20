5f5ebfb1f9beed53f80441d717b09f70f7c32902
gogojojo <joseph.gochua@gmail.com>
2015-03-20 16:12:09 -0700

    added modal for emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317174049) do
+ActiveRecord::Schema.define(version: 20150320225953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150317174049) do
   create_table "dots", force: :cascade do |t|
     t.integer  "dotid"
     t.integer  "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
