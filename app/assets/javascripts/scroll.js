0fd240d282cdff09952c9d80ddeac5ee5dfafb9b
7sun <pakrouse@gmail.com>
2015-03-20 20:41:30 -0700

    fixes wallpaper download

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -111,7 +111,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $( "#to-quilt" ).click(function() {
-    $(".main").moveDown();
+    // $(".main").moveDown();
     $( "#quilt .quilt-patch:nth-last-child(2)" ).addClass('reveal');
   });
 });
